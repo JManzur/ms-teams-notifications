ca1878723edc95a983a648f45e6f78f1ff5743ac
JManzur <jmanzurst@gmail.com>
2023-02-03 13:14:17 -0300

    Fri Feb  3 13:14:16 -03 2023

--- modules/slack-notifications/cloudwatch.tf
+++ modules/slack-notifications/cloudwatch.tf
@@ -15,14 +15,14 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_utilization" {
   period              = local.period
   statistic           = local.statistic
   threshold           = local.threshold
-  treat_missing_data  = "notBreaching" #missing, ignore, breaching, notBreaching. https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html
+  treat_missing_data  = "missing" #missing, ignore, breaching, notBreaching. https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/AlarmThatSendsEmail.html
   alarm_description   = "Monitors EC2 ${var.instance_id} ${local.statistic} ${local.metric_name}. Alarm will trigger if ${local.metric_name} exceeds ${local.threshold}% during ${local.evaluation_periods} evaluation periods of ${local.period} seconds"
 
   dimensions = {
     InstanceId = var.instance_id
   }
 
-  insufficient_data_actions = [aws_sns_topic.slack_notification.arn]
+  #insufficient_data_actions = [aws_sns_topic.ms_teams_notification.arn] #Not recomended for CPUUtilization
   ok_actions                = [aws_sns_topic.slack_notification.arn]
   alarm_actions             = [aws_sns_topic.slack_notification.arn]
 }
